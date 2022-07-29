9dc83ea4acfdc47a6077e10e4895c494da429146
slrnd <strmbld@gmail.com>
2022-07-29 09:24:00 +0300

    add client, gen dto models with nswag

---
+++ Movies.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Movies.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
